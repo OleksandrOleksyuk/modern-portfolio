1543c26a53ae59d831115c40948a593f22a33ffd
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-12-02 07:37:42 +0100

    create style theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [".src/**/*.{ts, tsx}", "./index.html"],
+  content: ["./src/**/*.tsx", "./src/**/*.ts", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white-primary": "#F0F2F5",
+        "white-secondary": "#FFF",
+        "black-primary": "#161513",
+        "black-secondary": "#1C1C22",
+      },
+    },
   },
   plugins: [],
 };
