1170da8d12d5b60c38a836b58e5ac1a8786ff87e
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-12-02 06:54:24 +0100

    add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
   plugins: [react()],
 });
