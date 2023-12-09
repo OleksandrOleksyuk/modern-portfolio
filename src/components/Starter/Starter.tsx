3f9805cc9f07c427a2f0e580638526f768876bde
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-12-09 12:17:25 +0100

    add another animation

--- src/components/Starter/Starter.tsx
+++ src/components/Starter/Starter.tsx
@@ -7,16 +7,16 @@ const Starter = () => {
   useEffect(() => {
     const loadingTimeout = setTimeout(() => {
       navigate("/home");
-    }, 3000);
+    }, 5000);
     return () => clearTimeout(loadingTimeout);
   }, [navigate]);
 
   return (
-    <div className="bg-black h-screen text-white flex flex-col justify-center items-center">
-      <h1>Oleksandr Oleksyuk</h1>
+    <div className="bg-black h-screen text-gray-300 flex flex-col justify-center items-center overflow-hidden">
+      <h1 className="fadeInScale uppercase">Oleksandr Oleksyuk</h1>
       <div className="w-full h-auto flex justify-center items-center">
-        <div className="w-3/5 h-3 border-[1px] border-white rounded overflow-hidden">
-          <div className="h-3 bg-white loading-starter"></div>
+        <div className="w-3/5 h-1 border-[1px] border-gray-800 rounded-sm overflow-hidden loading-line-box-container">
+          <div className="h-1 loading-starter"></div>
         </div>
       </div>
     </div>
